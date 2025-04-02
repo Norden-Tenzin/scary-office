9c03b7c06a88ed0d7ea7b64dc826456a7208c160
Norden-Tenzin <nordentenzin29@gmail.com>
2025-04-02 11:55:30 +0530

    added some spice

--- components/look_component/look_component.gd
+++ components/look_component/look_component.gd
@@ -2,11 +2,34 @@ extends Node3D
 
 @export var head: Node3D
 @export var camera: Camera3D
+@export var hands: Node3D
 
-@export var SENSITIVITY: float = 0.03
+var head_y_axis: float = 0.0
+var camera_x_axis: float = 0.0
+
+@export var SENSITIVITY: float = 0.25
+@export var CAMERA_ACCELERATION: float = 2
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		head.rotate_y(-event.relative.x * SENSITIVITY)
-		camera.rotate_x(event.relative.y * SENSITIVITY)
-		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+		head_y_axis += event.relative.x * SENSITIVITY
+		
+		var raw_target_rotation: float = -deg_to_rad(head_y_axis)
+		var current_rotation: float = head.rotation.y
+		
+		var rotation_difference: float = raw_target_rotation - current_rotation
+		rotation_difference = fposmod(rotation_difference + PI, 2 * PI) - PI
+		var max_rotation_change: float = deg_to_rad(90)
+		var clamped_difference: float = clamp(rotation_difference, -max_rotation_change, max_rotation_change)
+		var constrained_target: float = current_rotation + clamped_difference
+		head_y_axis = -rad_to_deg(constrained_target)
+		camera_x_axis += event.relative.y * SENSITIVITY
+		camera_x_axis = clamp(camera_x_axis, -40, 80)
+
+func _process(delta: float) -> void:
+	head.rotation.y = lerp_angle(head.rotation.y, -deg_to_rad(head_y_axis), CAMERA_ACCELERATION * delta)
+	camera.rotation.x = lerp(camera.rotation.x, -deg_to_rad(camera_x_axis), CAMERA_ACCELERATION * delta)
+	camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(85))
+	
+	hands.rotation.y = -deg_to_rad(head_y_axis)
+	hands.rotation.x = -deg_to_rad(camera_x_axis)
--- components/movement_component/movement_component.gd
+++ components/movement_component/movement_component.gd
@@ -24,7 +24,7 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "up", "down")
-	var direction := (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (head.basis.x * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		player.velocity.x = direction.x * SPEED
 		player.velocity.z = direction.z * SPEED

e31fa85b3a7d70ea94783f068111597d51d2dbe8
Norden-Tenzin <nordentenzin29@gmail.com>
2025-04-02 12:23:34 +0530

    movement fix

--- components/movement_component/movement_component.gd
+++ components/movement_component/movement_component.gd
@@ -14,7 +14,6 @@ var head_base_pos: Vector3 = Vector3.ZERO
 
 func _ready() -> void:
 	head_base_pos = head.position
-	print(head_base_pos)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -24,7 +23,7 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "up", "down")
-	var direction := (head.basis.x * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		player.velocity.x = direction.x * SPEED
 		player.velocity.z = direction.z * SPEED

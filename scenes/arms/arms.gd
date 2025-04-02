61101009cf1558c7e1dba78e78ce76cc1b51413a
Norden-Tenzin <nordentenzin29@gmail.com>
2025-04-02 22:57:37 +0530

    bug fix for dropping

--- scenes/arms/arms.gd
+++ scenes/arms/arms.gd
@@ -13,14 +13,14 @@ func _on_pick_up_component_item_picked_up(node_path: NodePath, with_hand: Global
 func _on_pick_up_component_item_dropped(with_hand: GlobalEnums.Hand) -> void:
 	match with_hand: 
 		GlobalEnums.Hand.Left:
-			var obj: RigidBody3D = get_node(remote_left.remote_path)
-			print("OBJ")
-			print(obj)
-			remote_left.remote_path = NodePath()
-			obj.freeze = false
-			obj.apply_impulse(Vector3.UP * 3)
+			if remote_left.remote_path != NodePath():
+				var obj: RigidBody3D = get_node(remote_left.remote_path)
+				remote_left.remote_path = NodePath()
+				obj.freeze = false
+				obj.apply_impulse(Vector3.UP * 3)
 		GlobalEnums.Hand.Right:
-			var obj: RigidBody3D = get_node(remote_right.remote_path)
-			remote_right.remote_path = NodePath()
-			obj.freeze = false
-			obj.apply_impulse(Vector3.UP * 3)
+			if remote_right.remote_path != NodePath():
+				var obj: RigidBody3D = get_node(remote_right.remote_path)
+				remote_right.remote_path = NodePath()
+				obj.freeze = false
+				obj.apply_impulse(Vector3.UP * 3)
